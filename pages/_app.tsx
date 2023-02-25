2ee1e674d6a69a739b4da3b108fd48f4a8c6d0f0
chrisbrights <softtalents1986@outlook.com>
2023-02-25 13:28:46 +0900

    working on homepage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { WagmiConfig, createClient, configureChains} from 'wagmi'
+import { publicProvider } from 'wagmi/providers/public'
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { mainnet, goerli } from 'wagmi/chains'
+
+const { provider, webSocketProvider } = configureChains(
+  [mainnet, goerli],
+  [
+    publicProvider(),
+    alchemyProvider({ apiKey: process.env.ALCHEMY_KEY || ''})
+  ],
+)
+
+const client = createClient({
+  autoConnect: true,
+  provider,
+  webSocketProvider,
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={client}>
+      <Component {...pageProps} />
+    </WagmiConfig>
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,51 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import { useState } from 'react'
+import { useAccount, useBalance, useConnect, useEnsName } from 'wagmi'
+import { Loader, Button } from '../components';
 
 export default function Home() {
+  const [showWalletOptions, setShowWalletOptions] = useState(false);
+
+  const { address, isConnected, isConnecting, isReconnecting } = useAccount();
+
+  const { data: balanceData, isError, isLoading } = useBalance({
+    address: address,
+    chainId: 5 // goerli
+  })
+
+  const accountLoading = (isConnecting || isReconnecting);
+
+  const loading = (accountLoading || isLoading) && !balanceData;
+
+  const renderContent = () => {
+    if (loading) return <Loader size={8} />;
+    if (balanceData) {
+      return (
+        <>
+          <h1 className="mb-8 text-4xl font-bold">My Wallet</h1>
+          <div className="inline-flex place-items-center">
+            <h6 className="ml-2 text-2xl">{`Ξ ${Number(
+              balanceData?.formatted
+            ).toFixed(4)} ${balanceData?.symbol}`}</h6>
+          </div>
+        </>
+      );
+    }
+
+    return (
+      <>
+        <h1 className="mb-8 text-4xl font-bold">
+          Welcome to the NextJS wagmi template!
+        </h1>
+        <Button
+          loading={accountLoading}
+          onClick={() => setShowWalletOptions(true)}
+        >
+          Connect to Wallet
+        </Button>
+      </>
+    );
+  };
+
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
